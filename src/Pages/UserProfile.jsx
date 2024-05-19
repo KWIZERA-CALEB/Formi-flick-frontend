541d11cd47d3d267d067355990fc16f30afb6d54
Kwizera Caleb <your email\a>
2024-05-19 19:45:06 +0200

    Made new changes on profiles

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -8,9 +8,9 @@ const UserProfile = () => {
     <>
         <Navigation  />
         <div className="p-[40px] flex flex-col md:flex-row">
-            <div className="bg-accentBg w-full mt-[22px] h-[387.969px] pt-[20px] pl-[40px] pr-[40px] pb-[20px] rounded-[20px] flex flex-row space-x-[20px] items-center md:w-[50%]">
+            <div className="bg-accentBg w-full mt-[22px] h-[387.969px] pt-[20px] pl-[40px] pr-[40px] pb-[20px] rounded-[20px] flex flex-col  space-x-[20px] items-center  md:flex-row md:w-[50%]">
                 <div>
-                    <img src="/images/3d_3.png" className="w-[60px] h-[60px] rounded-full object-cover object-[50% 50%] cursor-pointer outline outline-solid outline-offset-2 outline-cyan-500" alt="Profile" />
+                    <img src="/images/3d_3.png" className="w-[60px] h-[60px] rounded-[30px] object-cover object-[50% 50%] cursor-pointer outline outline-solid outline-offset-2 outline-cyan-500" alt="Profile" />
                 </div>
                 <div className="flex flex-col space-y-[-80px]">
                     <div>
