b25851c4b25ca792a7bbbb4a837912cd2a15cd71
Kwizera Caleb <your email\a>
2024-05-24 15:10:18 +0200

    Made new adjustments on skeleton loading

--- src/Components/MovieContainer.jsx
+++ src/Components/MovieContainer.jsx
@@ -28,6 +28,7 @@ const MovieContainer = () => {
     <>
       
       {loading ? 
+        <>
           <div className="rounded-[10px] shadow-md">
               <div className="relative skeleton animate-pulse">
                   <Link to={'/movie'}>
@@ -41,6 +42,100 @@ const MovieContainer = () => {
                 <div className="skeleton animate-pulse p-[20px] mt-[10px] bg-accentBg rounded-[10px] text-[#fff]/[90%] cursor-pointer hover:text-cyan-500/[100%] select-none"><p>The Black Clover</p></div>
               </Link>
           </div>
+          <div className="rounded-[10px] shadow-md">
+              <div className="relative skeleton animate-pulse">
+                  <Link to={'/movie'}>
+                    <img src="/images/bannerimg.jpg" className="object-cover rounded-[10px] mb-[3px] select-none cursor-pointer hovered-image" alt="Movieimage" />
+                  </Link>
+                  <Link to={'/movie'}>
+                    <div className="w-[40px] h-[40px] text-cyan-500 absolute top-[20px] left-[20px] cursor-pointer hover:animate-spin hover:w-[42px] hover:h-[42px]"><PlayIcon /></div>
+                  </Link>
+              </div>
+              <Link to={'/movie'}>
+                <div className="skeleton animate-pulse p-[20px] mt-[10px] bg-accentBg rounded-[10px] text-[#fff]/[90%] cursor-pointer hover:text-cyan-500/[100%] select-none"><p>The Black Clover</p></div>
+              </Link>
+          </div>
+          <div className="rounded-[10px] shadow-md">
+              <div className="relative skeleton animate-pulse">
+                  <Link to={'/movie'}>
+                    <img src="/images/bannerimg.jpg" className="object-cover rounded-[10px] mb-[3px] select-none cursor-pointer hovered-image" alt="Movieimage" />
+                  </Link>
+                  <Link to={'/movie'}>
+                    <div className="w-[40px] h-[40px] text-cyan-500 absolute top-[20px] left-[20px] cursor-pointer hover:animate-spin hover:w-[42px] hover:h-[42px]"><PlayIcon /></div>
+                  </Link>
+              </div>
+              <Link to={'/movie'}>
+                <div className="skeleton animate-pulse p-[20px] mt-[10px] bg-accentBg rounded-[10px] text-[#fff]/[90%] cursor-pointer hover:text-cyan-500/[100%] select-none"><p>The Black Clover</p></div>
+              </Link>
+          </div>
+          <div className="rounded-[10px] shadow-md">
+              <div className="relative skeleton animate-pulse">
+                  <Link to={'/movie'}>
+                    <img src="/images/bannerimg.jpg" className="object-cover rounded-[10px] mb-[3px] select-none cursor-pointer hovered-image" alt="Movieimage" />
+                  </Link>
+                  <Link to={'/movie'}>
+                    <div className="w-[40px] h-[40px] text-cyan-500 absolute top-[20px] left-[20px] cursor-pointer hover:animate-spin hover:w-[42px] hover:h-[42px]"><PlayIcon /></div>
+                  </Link>
+              </div>
+              <Link to={'/movie'}>
+                <div className="skeleton animate-pulse p-[20px] mt-[10px] bg-accentBg rounded-[10px] text-[#fff]/[90%] cursor-pointer hover:text-cyan-500/[100%] select-none"><p>The Black Clover</p></div>
+              </Link>
+          </div>
+          <div className="rounded-[10px] shadow-md">
+              <div className="relative skeleton animate-pulse">
+                  <Link to={'/movie'}>
+                    <img src="/images/bannerimg.jpg" className="object-cover rounded-[10px] mb-[3px] select-none cursor-pointer hovered-image" alt="Movieimage" />
+                  </Link>
+                  <Link to={'/movie'}>
+                    <div className="w-[40px] h-[40px] text-cyan-500 absolute top-[20px] left-[20px] cursor-pointer hover:animate-spin hover:w-[42px] hover:h-[42px]"><PlayIcon /></div>
+                  </Link>
+              </div>
+              <Link to={'/movie'}>
+                <div className="skeleton animate-pulse p-[20px] mt-[10px] bg-accentBg rounded-[10px] text-[#fff]/[90%] cursor-pointer hover:text-cyan-500/[100%] select-none"><p>The Black Clover</p></div>
+              </Link>
+          </div>
+          <div className="rounded-[10px] shadow-md">
+              <div className="relative skeleton animate-pulse">
+                  <Link to={'/movie'}>
+                    <img src="/images/bannerimg.jpg" className="object-cover rounded-[10px] mb-[3px] select-none cursor-pointer hovered-image" alt="Movieimage" />
+                  </Link>
+                  <Link to={'/movie'}>
+                    <div className="w-[40px] h-[40px] text-cyan-500 absolute top-[20px] left-[20px] cursor-pointer hover:animate-spin hover:w-[42px] hover:h-[42px]"><PlayIcon /></div>
+                  </Link>
+              </div>
+              <Link to={'/movie'}>
+                <div className="skeleton animate-pulse p-[20px] mt-[10px] bg-accentBg rounded-[10px] text-[#fff]/[90%] cursor-pointer hover:text-cyan-500/[100%] select-none"><p>The Black Clover</p></div>
+              </Link>
+          </div>
+          <div className="rounded-[10px] shadow-md">
+              <div className="relative skeleton animate-pulse">
+                  <Link to={'/movie'}>
+                    <img src="/images/bannerimg.jpg" className="object-cover rounded-[10px] mb-[3px] select-none cursor-pointer hovered-image" alt="Movieimage" />
+                  </Link>
+                  <Link to={'/movie'}>
+                    <div className="w-[40px] h-[40px] text-cyan-500 absolute top-[20px] left-[20px] cursor-pointer hover:animate-spin hover:w-[42px] hover:h-[42px]"><PlayIcon /></div>
+                  </Link>
+              </div>
+              <Link to={'/movie'}>
+                <div className="skeleton animate-pulse p-[20px] mt-[10px] bg-accentBg rounded-[10px] text-[#fff]/[90%] cursor-pointer hover:text-cyan-500/[100%] select-none"><p>The Black Clover</p></div>
+              </Link>
+          </div>
+          <div className="rounded-[10px] shadow-md">
+              <div className="relative skeleton animate-pulse">
+                  <Link to={'/movie'}>
+                    <img src="/images/bannerimg.jpg" className="object-cover rounded-[10px] mb-[3px] select-none cursor-pointer hovered-image" alt="Movieimage" />
+                  </Link>
+                  <Link to={'/movie'}>
+                    <div className="w-[40px] h-[40px] text-cyan-500 absolute top-[20px] left-[20px] cursor-pointer hover:animate-spin hover:w-[42px] hover:h-[42px]"><PlayIcon /></div>
+                  </Link>
+              </div>
+              <Link to={'/movie'}>
+                <div className="skeleton animate-pulse p-[20px] mt-[10px] bg-accentBg rounded-[10px] text-[#fff]/[90%] cursor-pointer hover:text-cyan-500/[100%] select-none"><p>The Black Clover</p></div>
+              </Link>
+          </div>
+        
+        </>
+          
       
       : 
         <>
